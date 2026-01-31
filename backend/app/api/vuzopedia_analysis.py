3d430d8e39ad727c7a14200ca090c656aa797566
Vlasova Mariia <vlasovam014@gmail.com>
2026-01-31 23:52:38 +0300

    add service for vuzopedia

--- backend/app/api/vuzopedia_analysis.py
+++ backend/app/api/vuzopedia_analysis.py
@@ -1,44 +1,38 @@
 from typing import Optional
 
-from fastapi import APIRouter, Query
+from app.db.session import get_db
+from app.services import vuzopedia_service
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
 
-@router.get("/search")
-async def search_vuzopedia_programs(
-    q: Optional[str] = Query("", description="Поисковый запрос"),
-    university: Optional[str] = Query(None, description="Фильтр по университету"),
-    min_score: Optional[int] = Query(None, description="Минимальный проходной балл"),
+@router.get("/")
+async def get_vuzopedia_programs(
+    db: AsyncSession = Depends(get_db),
     max_cost: Optional[int] = Query(None, description="Максимальная стоимость"),
+    min_score: Optional[int] = Query(None, description="Минимальный проходной балл"),
+    q: Optional[str] = Query(None, description="Поисковый запрос"),
+    page: int = Query(1, description="Номер страницы"),
+    size: int = Query(100, description="Размер страницы"),
 ):
     """Поиск образовательных программ"""
-    filters = {}
-    if university:
-        filters["university"] = university
-    if min_score:
-        filters["min_score"] = min_score
-    if max_cost:
-        filters["max_cost"] = max_cost
 
-    if q:
-        programs = [{"title": "program 1", "price": 100}]
-        # programs = vuzopedia_service.search_programs(q, filters)
-    else:
-        programs = [{"title": "program 1", "price": 100}, {"title": "program 2", "price": 50}]
-        # programs = vuzopedia_service.get_all_programs(filters)
-
-    return {"query": q, "count": len(programs), "programs": programs, "filters": filters}
-
-
-@router.get("/visualise")
-async def visualise_vuzopedia_program_feature(program_id: int, feature: str):
-    """Визуализация метрики образовательной программы"""
-    return {"visualisation": "complete"}
+    return await vuzopedia_service.get_vuzopedia_programs_service(
+        db=db,
+        max_cost=max_cost,
+        min_score=min_score,
+        q=q,
+        page=page,
+        size=size,
+    )
 
 
 @router.get("/{program_id}")
-async def get_vuzopedia_program_by_id(program_id: int):
+async def get_vuzopedia_program_by_id(
+    program_id: int,
+    db: AsyncSession = Depends(get_db),
+):
     """Получить программу из Vuzopedia по ID"""
-    program = {"title": "program 1", "price": 100}
-    return program
+    return await vuzopedia_service.get_vuzopedia_program_by_id_service(program_id=program_id, db=db)
