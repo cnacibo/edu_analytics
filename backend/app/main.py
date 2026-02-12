68a67059ebb50d0c3438d579197e725bcb47cb70
Vlasova Mariia <vlasovam014@gmail.com>
2026-02-12 22:16:31 +0300

    update docker and add cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from app.api import hse_analysis, vuzopedia_analysis
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Edu Analytics API",
@@ -9,6 +10,20 @@ app = FastAPI(
 
 app.include_router(hse_analysis.router, prefix="/hse", tags=["HSE Analysis"])
 app.include_router(vuzopedia_analysis.router, prefix="/vuzopedia", tags=["Vuzopedia Analysis"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3000",
+        "http://localhost:8080",
+        "http://0.0.0.0:8080",
+    ],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health", tags=["Health"])
