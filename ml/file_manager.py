273f3b51d70f90e2c6ad1c243e4ac0f5cbb20a4d
Ledentsova Viktoria <ledentsovaicloud.com@MacBook-Air-Ledentsova.local>
2026-03-03 01:11:02 +0300

    added tag extraction functionality

---
+++ ml/file_manager.py
@@ -0,0 +1,74 @@
+"""
+Метод, выполняющий
+- Чтение CSV файл с колонками title, content и results
+- Запуск процесса обработки текста и извлечение из него тегов-
+- Сохранение результат в файл tags.csv.
+"""
+
+
+import pandas as pd
+from tag_extractor import TagExtractor
+from pathlib import Path
+
+def process_csv_file(input_file: str, output_file: str = None):
+    """
+    Суть: Читает CSV, извлекает теги, сохраняет результат.
+
+    Ожидает CSV с колонками:
+    - title (название дисциплины)
+    - content (содержание дисциплины)
+    - results (планируемые результаты)
+
+    Ожидаемый результат:
+    - Файл tags.csv с двумя колонками title, tags
+    """
+    df = pd.read_csv(input_file)
+    # print(f"Найдено строк: {len(df)}") -> log
+
+    content_col = None
+    results_col = None
+    title_col = None
+
+    for col in df.columns:
+        col_lower = col.lower()
+        if 'content' in col_lower:
+            content_col = col
+        elif 'results' in col_lower:
+            results_col = col
+        elif 'title' in col_lower:
+            title_col = col
+
+    if not content_col or not results_col:
+        print("Не найдены нужные колонки! \nДоступные колонки: {list(df.columns)}")
+        return
+
+
+    extractor = TagExtractor()
+    all_tags = []
+
+    for idx, row in df.iterrows():
+        prog_name = row[content_col]
+        description = row[results_col]
+
+        if pd.isna(description) or not isinstance(description, str) or not description.strip():
+            print("  Пустое описание, пропуск")
+            all_tags.append([])
+            continue
+
+        tags = extractor.extract(description)
+        all_tags.append(tags)
+    df['tags'] = all_tags
+    df['tags_str'] = df['tags'].apply(lambda x: ', '.join(x) if x else '')
+
+    if output_file is None:
+        input_path = Path(input_file)
+        # output_file = input_path.parent / f"tags{input_path.suffix}"
+        script_dir = Path(__file__).parent
+        output_file = script_dir / "tags.csv"
+
+    if title_col is None:
+        title_col = content_col
+    df_to_save = df[[title_col, 'tags_str']].copy()
+    df_to_save.to_csv(output_file, index=False, encoding='utf-8-sig')
+    # print(f"\nРезультат сохранён в: {output_file}") -> log
+    return df
